f4fa32b05d0331c4ce1c8cc2f0bfaa71644457a1
PeiJu-Hsu <j40024@gmail.com>
2023-11-17 13:09:20 +0800

    Add: set global style
    Reset style and create global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
 import { useState } from "react";
+import { createGlobalStyle } from "styled-components";
+import { Reset } from "styled-reset";
 // import "./App.css";
 // import reactLogo from "./assets/react.svg";
 // import viteLogo from "/vite.svg";
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
 
+  /* body {
+    font-family: 'Noto Sans TC', sans-serif;
+  }
+
+  #root {
+    min-height: 100vh;
+    padding: 140px 0 115px;
+    position: relative;
+
+    @media screen and (max-width: 1279px) {
+      padding: 102px 0 208px;
+    }
+  } */
+`;
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+      <Reset />
+      <GlobalStyle />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           {/* <img src={viteLogo} className="logo" alt="Vite logo" /> */}
